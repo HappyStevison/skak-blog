e15893f49362f668646bd23286ccf43e07e06f1c
HappyStevison <happystevison@gmail.com>
2025-09-23 00:45:08 +0200

    Updates

---
+++ assets/2025-09-23-D4Knf1mD.js
@@ -0,0 +1,14 @@
+const e=`---
+title: Hello Chess World
+date: 2025-09-23
+excerpt: My very first chess post in React.
+---
+
+# Hello Chess World
+
+This blog is built with **React** and hosted on **GitHub Pages**.
+
+> Future topics:
+- Openings
+- Tactics
+- Endgames`;export{e as default};
